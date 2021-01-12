8aafdf36c685ef89e27b0b4e5e5f9d227f873d24
Harry Akeroyd <thirtythreezero@outlook.com>
2021-01-12 19:31:17 +1000

    Finished code, to clean

---
+++ Lifeguard_Monte_Carlo_Sim.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tuesday 12 January 2021.
+
+@author: h. thirtythreezero@outlook.com
+
+References:
+https://www.youtube.com/watch?v=et4tLWaINFY&ab_channel=purdueMET
+
+"""
+#------------------------------------------------------------------------------#
+#------------------------------------------------------------------------------#
+#------------------------------------------------------------------------------#
+# Libraries.
+import matplotlib.pyplot as plt
+import math
+import random
+#------------------------------------------------------------------------------#
+#------------------------------------------------------------------------------#
+#------------------------------------------------------------------------------#
+def obj_fnc():
+    """
+    """
+    to_plot = []
+
+    for x in list(range(0,200)):
+        time_trav = (x/15) + ((math.sqrt(2500+((200-x)*(200-x))))/5)
+        to_plot.append(time_trav)
+
+        enter_water = to_plot.index(min(to_plot))
+
+    print('HARDCODED ABSOLUTE VALUES:')
+    print(f'The Lifeguard should enter the water at {enter_water}m down shore.')
+    print(f'The Lifeguard will reach the swimmer in {min(to_plot):.2f}sec.')
+    return to_plot, enter_water
+#------------------------------------------------------------------------------#
+#------------------------------------------------------------------------------#
+#------------------------------------------------------------------------------#
+def monte_carlo_sim():
+    """
+    """
+    rand_list = []
+    sorted_rand_plot = []
+
+    for i in range(0, 200):
+        n = random.randint(0,200)
+        rand_list.append(n)
+
+    sorted_rand_list = sorted(rand_list)
+
+    for x in sorted_rand_list:
+        time_trav = (x/15) + ((math.sqrt(2500+((200-x)*(200-x))))/5)
+        sorted_rand_plot.append(time_trav)
+
+        sim_enter_water = sorted_rand_plot.index(min(sorted_rand_plot))
+
+    print('\nMONTE-CARLO SIMULATED VALUES:')
+    print(f'The Lifeguard should enter the water at {sim_enter_water}m down shore.')
+    print(f'The Lifeguard will reach the swimmer in {min(sorted_rand_plot):.2f}sec.')
+
+    return sorted_rand_plot, sim_enter_water
+#------------------------------------------------------------------------------#
+#------------------------------------------------------------------------------#
+#------------------------------------------------------------------------------#
+def plot_sim():
+    """
+    """
+    data = obj_fnc()
+
+    MCS = monte_carlo_sim()
+
+    plt.plot(data[0], label='Running over Swimming')
+    plt.plot(data[1], min(data[0]), 'r+', label='Optimum')
+    plt.plot(MCS[0], 'g', label='Monte Carlo Simulation')
+
+    plt.title('Lifeguard Problem for Monte Carlo Simulation')
+    plt.xlabel('Distance to Enter Water (m)')
+    plt.ylabel('Time to Reach Swimmer (s)')
+    plt.legend()
+
+    plt.show()
+#------------------------------------------------------------------------------#
+#------------------------------------------------------------------------------#
+#------------------------------------------------------------------------------#
+if __name__ == '__main__':
+    """ What does this do?
+    """
+    plot_sim()
+#------------------------------------------------------------------------------#
+#------------------------------------------------------------------------------#
+#------------------------------------------------------------------------------#
+#------------------------------------------------------------------------------#
+#------------------------------------------------------------------------------#
